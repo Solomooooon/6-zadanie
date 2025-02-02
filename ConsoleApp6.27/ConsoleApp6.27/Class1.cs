b8de16a24ee8474d89371b7399946ff60379bec4
Solomooooon <pptrvaleksandr@gmail.com>
2025-02-03 00:33:43 +0300

    Add files via upload

---
+++ ConsoleApp6.27/ConsoleApp6.27/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6._27
+{
+    class class1
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public class1(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        private double CD()
+        {
+            return B * B - 4 * A * C;
+        }
+
+        public bool Est()
+        {
+            double ds= CD();
+            return A < 0 && ds >= 0;
+        }
+    }
+}
---
+++ ConsoleApp6.27/ConsoleApp6.27/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6._27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            class1 E = new class1(-1, 3, 2);
+
+            if (E.Est())
+            {
+                Console.WriteLine("Уравнение имеет действительные корни.");
+            }
+            else
+            {
+                Console.WriteLine("Уравнение не имеет действительных корней или коэффициент a не меньше 0.");
+            }
+        }
+    }   
+}
